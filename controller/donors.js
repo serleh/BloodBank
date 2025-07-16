8ed9929322b17e588c2065f09e60441e1ca55486
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-07-16 18:50:49 +0100

    Added search functionality

--- controller/donors.js
+++ controller/donors.js
@@ -1,4 +1,5 @@
 const Donor = require("../models/Donor");
+const Contact = require("../models/Contact");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -30,6 +31,35 @@ module.exports = {
         }
       );
       res.redirect("/donors/profile");
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  getSearchDonor: (req, res) => {
+    res.render("search.ejs");
+  },
+
+  searchDonor: async (req, res) => {
+    const { city, bloodGroup } = req.body;
+    try {
+      const donors = await Donor.find({
+        city: { $regex: new RegExp(city, "i") },
+        bloodGroup: bloodGroup,
+      });
+
+      if (donors.length === 0) {
+        const fallbackContacts = await Contact.find({
+          city: { $ne: city },
+        }).limit(3);
+
+        return res.render("searchResults.ejs", {
+          message: "SORRY DONORS ARE NOT AVAILABLE IN THIS CITY.",
+          donors: [],
+          fallbackContacts,
+        });
+      }
+
+      res.render("searchResults.ejs", { donors, message: null });
     } catch (err) {
       console.error(err);
     }
--- routes/donors.js
+++ routes/donors.js
@@ -5,5 +5,7 @@ const { isLoggedIn } = require("../middleware/isLoggedIn");
 router.get("/profile", isLoggedIn, donorsController.getProfile);
 router.get("/profile/edit", isLoggedIn, donorsController.getUpdateProfile);
 router.post("/profile/edit", isLoggedIn, donorsController.updateProfile);
+router.get("/search", donorsController.getSearchDonor);
+router.post("/search", donorsController.searchDonor);
 
 module.exports = router;
